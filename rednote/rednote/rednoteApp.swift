7a8482e0a2c13705241817cf76f34ba6dd785e05
卤蛋君 <76891972@qq.com>
2026-04-14 10:48:17 +0800

    1.创建小红书项目

---
+++ rednote/rednote/rednoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  rednoteApp.swift
+//  rednote
+//
+//  Created by Mac Mini on 2026/4/14.
+//
+
+import SwiftUI
+
+@main
+struct rednoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
